0c34aaabe36aeb3c0f96a39c99dcce04300a251a
Binip Ghimire <binipghimire714@gmail.com>
2022-12-13 17:49:40 +0545

    transfer

---
+++ day5.js
@@ -0,0 +1,81 @@
+// single line comment
+// declaring variables
+// global scope
+// var a=1;
+// // cannot redeclare
+// // can reassign
+// let b=2;
+// // cannot reassign value
+// const c=10;
+// alert (a+b);
+// // number of data type in js
+// // 1. number data type
+// let a=5;
+// // 2. string data types
+// let d="string";
+// 3. Boolean ==> true or false
+// 4. Null
+// 5. Undefined
+// let z;
+// alert(z);
+// user input
+// let test;
+// prompt(test);
+// operators
+// 1. arithmetic operators
+// let a=10;
+// let b=20;
+// let c=(a+b);
+// alert (c);
+// alert(a+b);
+// alert(a-b);
+// alert(a*b);
+// 2. comparison operator
+// let a=10;
+// let b=10;
+// let d=a/b;
+// alert(a==b);
+// double equals used to check the value only
+// triple equals used to check the value and datatype
+// 3. logical operator
+// let isLoggedIn=true;
+// let isSuperUser=true;
+// // // logical and
+// let t=isLoggedIn && true;
+// // // logical or
+// let test = false||true;
+// // // // logical not
+//  let test1 =!true; 
+//   alert (t);
+// // // 4. type operator
+// // alert (typeof b);
+// // if conditional statement
+// if (10>12){
+//     alert(typeof b);
+// }else{
+// alert("test");
+// }
+// // array
+let array =[1, 2, 3, 4, 5, 6, 7];
+console.log(array[2]);
+// object
+let object1={name:"Binip",
+ age:24
+}
+console.log(object1.name, "object1");
+console.log(object1["age"], "object1");
+let car={
+    name:"Hyundai Creta",
+    model:"2020",
+    speed:"250 km/hr",
+    milage:"14km/l"
+}
+console.log(car["name"]);
+console.log(car.speed);
+console.log(car.milage);
+console.log(car["name"], car.speed, car.milage, car.model);
+
+
+
+
+
