048262d90682dfd2b03f0799fffcabf66e00d537
Binip Ghimire <binipghimire714@gmail.com>
2022-12-14 17:23:19 +0545

    function added

--- day6.js
+++ day6.js
@@ -1,24 +1,43 @@
-// person1. name equals to my name
-let person={
-    person1:"Binip", age:19
-    // person2:"Dipesh",
-};
-// if(person.person1!=="Binip")
+// // person1. name equals to my name
+// let person={
+//     person1:"Binip", age:19
+//     // person2:"Dipesh",
+// };
+// // if(person.person1!=="Binip")
+// // {
+// //     alert (person.person1);
+
+// // }  
+// // else{
+// //     alert(person.person2);
+
+// // }
+// if(person.person1==="Binip" && person.age>20)
 // {
-//     alert (person.person1);
-
-// }  
-// else{
-//     alert(person.person2);
-
+//     alert(person.person1);
 // }
-if(person.person1==="Binip" && person.age>20)
-{
-    alert(person.person1);
+// else if(person.person1==="Binip" && person.age<20){
+//     alert("failed to execute")
+// }
+// // function
+function add(a, b){
+    let c=a+b;
+     return alert(c);
+     let d=b+c;
+     alert(d);
+
 }
-else if(person.person1==="Binip" && person.age<20){
-    alert("failed to execute")
+add(10, 20);
+add(40, 50);
+function mul(a,b){
+    let c=a*b;
+    return alert (c);
 }
+mul(10,20);
+mul(40,50);
+
+
+
 
 
 
